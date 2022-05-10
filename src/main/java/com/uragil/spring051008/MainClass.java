864f3c5aad989ac3221b887f41a95e6e2c0a21a9
JBEDU29 <JBEDU29@DESKTOP-9PBMPIR>
2022-05-10 15:09:20 +0900

    2CTX ok  2Config_java err

---
+++ src/main/java/com/uragil/spring051008/MainClass.java
@@ -0,0 +1,40 @@
+package com.uragil.spring051008;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("개발하려는 xml 선택 dev / run");
+		Scanner scann = new Scanner(System.in);
+		String str = scann.next();
+		
+		String config = null;
+		
+		if(str.equals("dev")) {
+			config = "dev";
+		}else if(str.equals("run")) {
+			config = "run";
+		}
+		
+		scann.close();
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.getEnvironment().setActiveProfiles(config);
+		ctx.load("applicationCTX_dev.xml", "applicationCTX_run.xml");
+		
+		ServerInfo info =  ctx.getBean("serverInfo", ServerInfo.class);
+		System.out.println("server IP :" + info.getIpNum());
+		System.out.println("server Port :" + info.getPortNum());
+		
+		ctx.close();
+		
+	}
+
+}
---
+++ src/main/java/com/uragil/spring051008/MainClass_java.java
@@ -0,0 +1,42 @@
+package com.uragil.spring051008;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("개발하려는 xml 선택 dev / run");
+		Scanner scann = new Scanner(System.in);
+		String str = scann.next();
+		
+		String config = null;
+		
+		if(str.equals("dev")) {
+			config = "dev";
+		}else if(str.equals("run")) {
+			config = "run";
+		}
+		
+		scann.close();
+		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.getEnvironment().setActiveProfiles(config);
+		
+		ctx.register(ApplicationConfig_dev.class, ApplicationConfig_run.class);
+		
+		ServerInfo info =  ctx.getBean("ServerInfo", ServerInfo.class);
+		System.out.println("server IP :" + info.getIpNum());
+		System.out.println("server Port :" + info.getPortNum());
+		
+		ctx.close();
+		
+	}
+
+
+}
